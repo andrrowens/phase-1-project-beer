594e75d1646c9c3100f4a7f8883079595cb49d64
Adrian Barba <adrianbarba123@gmail.com>
2022-12-07 10:34:27 -0800

    testing

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const API = "https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10"
 fetch("https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10")
 .then(res => res.json())
 .then(data => console.log(data))
+
+const leftArrow = document.getElementById("left-arrow");
