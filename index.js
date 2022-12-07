ba3bfd0cd7fe89048bc95b3ee421801ea3a6617d
Andrew Owens <andrrowens@gmail.com>
2022-12-07 15:00:12 -0700

    new const

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const API = "https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10"
 fetch("https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10")
 .then(res => res.json())
 .then(data => console.log(data))
+
+const a = 6
