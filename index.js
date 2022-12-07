242f903b91a6273ece11f159b177ee6f73eb65dc
Adrian Barba <adrianbarba123@gmail.com>
2022-12-07 09:42:22 -0800

    modifying documents and creating more new files

---
+++ index.js
@@ -0,0 +1,5 @@
+const API = "https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10";
+
+fetch("https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10")
+.then(res => res.json())
+.then(data => console.log(data))
