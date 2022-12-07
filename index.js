be2678014170abf06ae54889681c4a1699b87b4f
Andrew Owens <andrrowens@gmail.com>
2022-12-07 13:00:20 -0700

    add click event to JS and add id to HTML

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const API = "https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10"
 fetch("https://api.openbrewerydb.org/breweries?by_city=denver&per_page=10")
 .then(res => res.json())
 .then(data => console.log(data))
+
+const leftArrow = document.getElementById("left-arrow")
+
+leftArrow.addEventListener("click", (e) => {
+    handleClick
+}
